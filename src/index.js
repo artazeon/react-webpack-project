fd22e592c4bbffc43b06989c927bdef26ce930bf
artazeon <artazeon@gmail.com>
2023-07-21 20:03:00 +0300

    install webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const title = 'React с Webpack и Babel';
+
+import App from './app';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<h1>{title}</h1>); 
