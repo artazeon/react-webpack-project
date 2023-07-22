6ab0769a74ce7ee2033d43c3f183124003bdc89f
artazeon <artazeon@gmail.com>
2023-07-22 17:34:26 +0300

    Hot module replacement в React

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client';
 
 const title = 'React с Webpack и Babel!';
 
-import App from './app';
+import App from './app'; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(<h1>{title}</h1>); 
